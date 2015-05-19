07b480b86e7f55bdd33b7198aeb9f06692018161
parahunter <parahunter@gmail.com>
2015-05-19 21:35:56 +0100

    more work on presentation

--- Assets/Presentation/Scripts/UPresenter/SlideManager.cs
+++ Assets/Presentation/Scripts/UPresenter/SlideManager.cs
@@ -4,28 +4,40 @@ using System.Collections.Generic;
 
 public class SlideManager : MonoBehaviour 
 {
-    public KeyCode forwardKey;
-    public KeyCode backKey;
+    public KeyCode forwardKey = KeyCode.RightArrow;
+    public KeyCode backKey = KeyCode.LeftArrow;
 
     public SlideCollection slideCollection;
     
     void Start()
     {
-        slideCollection.ShowFirst();
+        if(slideCollection != null)
+            slideCollection.ShowFirst();
     }
 
     void Update()
     {
         if(Input.GetKeyDown(forwardKey))
         {
-            slideCollection.NextSlide();
+            if (slideCollection != null)
+            {
+                if (!slideCollection.NextSlide())
+                    Application.LoadLevel(Application.loadedLevel + 1);
+            }
+            else
+                Application.LoadLevel(Application.loadedLevel + 1);
+
         }
         else if(Input.GetKeyDown(backKey))
         {
-            slideCollection.LastSlide();
+            if (slideCollection != null)
+            {
+                if (!slideCollection.LastSlide())
+                    Application.LoadLevel(Application.loadedLevel - 1);
+            }
+            else
+                Application.LoadLevel(Application.loadedLevel - 1);
+
         }
-
     }
-
-
 }
