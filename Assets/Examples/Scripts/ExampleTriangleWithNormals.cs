75f068c6fa42b03dba45a6d690e001c593744341
parahunter <parahunter@gmail.com>
2015-05-20 08:36:49 +0100

    work on triangle with normals

---
+++ Assets/Examples/Scripts/ExampleTriangleWithNormals.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ExampleTriangleWithNormals : MonoBehaviour 
+{
+
+    Mesh mesh;
+
+    void Start()
+    {
+        mesh = new Mesh();
+
+        //assign points
+        Vector3[] vertices = new Vector3[3];
+        vertices[0] = new Vector3(-1, -1, 0);
+        vertices[1] = new Vector3(0, 0.8f, 0);
+        vertices[2] = new Vector3(1, -1, 0);
+        mesh.vertices = vertices;
+
+        Vector3[] normals = new Vector3[3];
+        normals[0] = Vector3.back;
+        normals[1] = Vector3.back;
+        normals[2] = Vector3.back;
+        mesh.normals = normals;
+
+        //assign triangles
+        int[] triangles = new int[3] { 0, 1, 2 };
+        mesh.triangles = triangles;
+
+        GetComponent<MeshFilter>().mesh = mesh;
+    }
+}
---
+++ Assets/Examples/Scripts/Rotate.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Rotate : MonoBehaviour 
+{
+    public float rotationSpeed = 20f;
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+	}
+}
