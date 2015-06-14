d9e4fa1ffeb9771dce897aeff95604781fc28758
parahunter <parahunter@gmail.com>
2015-06-14 17:21:24 +0100

    square example

--- Assets/Examples/Scripts/ExampleBasicSquare.cs
+++ Assets/Examples/Scripts/ExampleBasicSquare.cs
@@ -2,27 +2,29 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class ExampleBasicTriangle : MonoBehaviour 
+public class ExampleBasicSquare : MonoBehaviour
 {
     Mesh mesh;
 
-	void Start () 
+    void Start()
     {
         mesh = new Mesh();
 
         //assign points
-        Vector3[] vertices = new Vector3[3];
+        Vector3[] vertices = new Vector3[4];
         vertices[0] = new Vector3(-1, -1, 0);
-        vertices[1] = new Vector3(0, 0.8f, 0);
-        vertices[2] = new Vector3(1, -1, 0);
+        vertices[1] = new Vector3(-1, 1, 0);
+        vertices[2] = new Vector3(1, 1, 0);
+        vertices[3] = new Vector3(1, -1, 0);
+
         mesh.vertices = vertices;
 
         //assign triangles
-        int[] triangles = new int[3] { 0, 1, 2 };
+        int[] triangles = new int[6] { 0, 1, 2, 2, 3, 0};
         mesh.triangles = triangles;
 
         GetComponent<MeshFilter>().mesh = mesh;
-	}
+    }
 }
 
 
