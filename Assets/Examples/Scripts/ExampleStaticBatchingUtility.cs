c2d28dc677605d257b52967e9994b3aeef1889ad
Alexander Birke <parahunter@gmail.com>
2016-10-27 12:46:35 +0100

    start on static batching example

---
+++ Assets/Examples/Scripts/ExampleStaticBatchingUtility.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleStaticBatchingUtility : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	void DoCombine()
+	{
+
+	}
+
+}
